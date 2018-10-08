e1572b24c7a9ceb9a211954541a01115370bb01e
jovan72 <bogoeskijovan@gmail.com>
2018-10-09 00:45:06 +0200

    Add files via upload

---
+++ SICKLMS511 reader/SICKLMS511 reader/SICKLMS511 reader/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace SICKLMS511_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textIPAddress = new System.Windows.Forms.TextBox();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.butConnect = new System.Windows.Forms.Button();
+            this.butGetData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textData = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.processDataBtn = new System.Windows.Forms.Button();
+            this.graphPicBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.graphPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textIPAddress
+            // 
+            this.textIPAddress.BackColor = System.Drawing.Color.LimeGreen;
+            this.textIPAddress.Location = new System.Drawing.Point(6, 41);
+            this.textIPAddress.Name = "textIPAddress";
+            this.textIPAddress.Size = new System.Drawing.Size(100, 20);
+            this.textIPAddress.TabIndex = 0;
+            this.textIPAddress.Text = "192.168.1.1";
+            this.textIPAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textIPAddress.TextChanged += new System.EventHandler(this.textIPAddress_TextChanged);
+            // 
+            // textPort
+            // 
+            this.textPort.BackColor = System.Drawing.Color.LimeGreen;
+            this.textPort.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textPort.Location = new System.Drawing.Point(137, 41);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(83, 20);
+            this.textPort.TabIndex = 1;
+            this.textPort.Text = "2111";
+            this.textPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // butConnect
+            // 
+            this.butConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butConnect.Location = new System.Drawing.Point(310, 38);
+            this.butConnect.Name = "butConnect";
+            this.butConnect.Size = new System.Drawing.Size(75, 23);
+            this.butConnect.TabIndex = 2;
+            this.butConnect.Text = "Connect";
+            this.butConnect.UseVisualStyleBackColor = true;
+            this.butConnect.Click += new System.EventHandler(this.butConnect_Click);
+            // 
+            // butGetData
+            // 
+            this.butGetData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butGetData.Location = new System.Drawing.Point(404, 38);
+            this.butGetData.Name = "butGetData";
+            this.butGetData.Size = new System.Drawing.Size(75, 23);
+            this.butGetData.TabIndex = 3;
+            this.butGetData.Text = "Get Data";
+            this.butGetData.UseVisualStyleBackColor = true;
+            this.butGetData.Click += new System.EventHandler(this.butGetData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Laser IP Address: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.butGetData);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.butConnect);
+            this.groupBox1.Controls.Add(this.textIPAddress);
+            this.groupBox1.Controls.Add(this.textPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(486, 73);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Laser Port: ";
+            // 
+            // textData
+            // 
+            this.textData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textData.Location = new System.Drawing.Point(12, 92);
+            this.textData.Multiline = true;
+            this.textData.Name = "textData";
+            this.textData.Size = new System.Drawing.Size(486, 258);
+            this.textData.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.processDataBtn);
+            this.groupBox2.Location = new System.Drawing.Point(504, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(368, 73);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // processDataBtn
+            // 
+            this.processDataBtn.Location = new System.Drawing.Point(6, 38);
+            this.processDataBtn.Name = "processDataBtn";
+            this.processDataBtn.Size = new System.Drawing.Size(119, 23);
+            this.processDataBtn.TabIndex = 0;
+            this.processDataBtn.Text = "Process Data";
+            this.processDataBtn.UseVisualStyleBackColor = true;
+            this.processDataBtn.Click += new System.EventHandler(this.processDataBtn_Click);
+            // 
+            // graphPicBox
+            // 
+            this.graphPicBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graphPicBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.graphPicBox.Location = new System.Drawing.Point(504, 92);
+            this.graphPicBox.Name = "graphPicBox";
+            this.graphPicBox.Size = new System.Drawing.Size(368, 258);
+            this.graphPicBox.TabIndex = 8;
+            this.graphPicBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(884, 362);
+            this.Controls.Add(this.graphPicBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textData);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(526, 400);
+            this.Name = "Form1";
+            this.Text = "LMS511 Reader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.graphPicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIPAddress;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.Button butConnect;
+        private System.Windows.Forms.Button butGetData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textData;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button processDataBtn;
+        private System.Windows.Forms.PictureBox graphPicBox;
+    }
+}
+
---
+++ SICKLMS511 reader/SICKLMS511 reader/SICKLMS511 reader/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using SICK_LMS5xx_PRO_Library;
+using SICK_Work;
+
+// ip adress of sensor 169.254.156.174
+
+namespace SICKLMS511_reader
+{
+    public partial class Form1 : Form
+    {
+        public bool CheckIPValidation = true;
+        private System.Net.IPAddress IPtest;
+        public SICK_Communication.Ethernet.TCP_client LMS_connection = new SICK_Communication.Ethernet.TCP_client();
+        public SICK_LMS5xx_PRO_Library.DataProcessing LMS_DataProcess = new SICK_LMS5xx_PRO_Library.DataProcessing();
+        public string LMS_Data;
+        public SICK_Work.SICK_Laserdevices.Scandata LMS_MEasurementStructure = new SICK_Work.SICK_Laserdevices.Scandata();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // function to check if device is online
+        public Boolean pingAddress(string ipadd)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply pingresult = ping.Send(ipadd);
+                if (pingresult.Status.ToString() == "Success")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+        private void butConnect_Click(object sender, EventArgs e)
+        {
+            if (butConnect.Text == "Connect")
+            {
+
+                // check if  IPaddress is valid and the device is online
+                if (CheckIPValidation == true & pingAddress(textIPAddress.Text) == true)
+                {
+                    Int32 portNum = Convert.ToInt32(textPort.Text);
+                    LMS_connection.Client_Connect(textIPAddress.Text, portNum); // connect the LMS511
+
+                    // enable the buttons
+                    butGetData.Enabled = true;
+                    textIPAddress.Enabled = false;
+
+                    butConnect.Text = "Disconnect";
+                }
+                else
+                {
+                    textData.Text = "Device not avaiable, confirm device ip address and port \n";
+                }
+            }
+            else
+            {
+                // Disconnect
+                LMS_connection.Client_Disconnect();
+                butGetData.Enabled = false;
+                textIPAddress.Enabled = true;
+                butConnect.Text = "Connect";
+            }
+        }
+
+        private void textIPAddress_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Net.IPAddress.TryParse(textIPAddress.Text, out IPtest) == true) // check if input text is an ip address
+            {
+                textIPAddress.BackColor = Color.Lime;
+                CheckIPValidation = true;
+            }
+            else
+            {
+                textIPAddress.BackColor = Color.Red;
+                CheckIPValidation = false;
+            }
+        }
+
+        private void butGetData_Click(object sender, EventArgs e)
+        {
+            textData.Text = "";
+
+            LMS_connection.SendString(SICK_LMS5xx_PRO_Library.DeviceFunctions.GetMeasurement(), SICK_Communication.Ethernet.TCP_client.FramingTypes.Stx_Etx);
+
+            // Wait for the Device to respond
+            while (!(LMS_connection.Available > 0))
+            {
+            }
+
+            // Read the raw string data from the sensor
+            LMS_Data = LMS_connection.Readavailable_string();
+
+            // Convert data string to the SICK_Work structure
+            //LMS_MEasurementStructure = LMS_DataProcess.Process_SRA_LMDscandata(LMS_Data);
+            //check for errors in data
+            //if (LMS_MEasurementStructure.Error_Occured == false)
+            //{
+            //    LMS_Data = "Error occur during data conversion\n";
+            //}
+
+            // Show the data in HEX format to textview
+
+            textData.Text = LMS_Data;
+            /*
+            int value;
+            LMS_MEasurementStructure = LMS_DataProcess.Process_SRA_LMDscandata(textData.Text);
+            ushort[] dist = LMS_MEasurementStructure.Dist1.DataPoints;
+            for(int j = 0; j < dist.Length; j++)
+            {
+                textData.Text = dist[j].ToString();
+
+            }
+            */
+
+
+        }
+        private void processDataBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(900, 600);
+            Pen pen = new Pen(Color.Silver, 1f);
+            Graphics g = Graphics.FromImage(bmp);
+            for (int ii = 0; ii < 900; ii += 50)
+                g.DrawLine(pen, ii, 0, ii, 599);
+            for (int ii = 0; ii < 600; ii += 50)
+                g.DrawLine(pen, 0, ii, 899, ii);
+
+            g.DrawLine(pen, 0, 599, 899, 599);
+            g.DrawLine(pen, 899, 0, 899, 599);
+
+            pen.Color = Color.Black;
+            pen.Width = 3f;
+
+            g.DrawLine(pen, 0, 550, 899, 550);
+            g.DrawLine(pen, 75, 0, 75, 599);
+
+            Font font = new Font("Cambria", 13);
+            for (int ii = 75; ii < 850; ii += 50)
+            {
+                g.DrawLine(pen, ii, 550, ii, 565);
+                if (ii != 75)
+                    g.DrawString("" + ((ii - 75) / 2), font, pen.Brush, new PointF(ii-12.5f, 570));
+            }
+
+            for (int ii = 0, jj = 550; jj > 0; jj -= 50, ii += 50)
+            {
+                g.DrawLine(pen, 65, jj, 75, jj);
+                if (ii != 0)
+                    g.DrawString("" + (ii/5), font, pen.Brush, new PointF(25, jj-10));
+            }
+
+            pen.Color = Color.Red;
+            g.DrawString("Angle", font, pen.Brush, new PointF(850, 555));
+            g.DrawString("Dist(dm)", font, pen.Brush, new PointF(5, 5));
+
+            string str = textData.Text;
+            string pointsStr = "";
+            if (str != null && str != "")
+            {
+
+                SICK_Laserdevices.Scandata data = LMS_DataProcess.Process_SRA_LMDscandata(str);
+                ushort[] dist = data.Dist1.DataPoints;       
+
+                pen.Color = Color.Magenta;
+                pen.Width = 1.5f;
+                float xp = 75f;
+
+                for (int ii=0; ii< dist.Length; ii++)
+                {
+                    float yp = 550 - (dist[ii] / 100f)*5;
+                    g.DrawEllipse(pen, xp - 2.5f, yp - 2.5f, 5, 5);
+                    
+                    pointsStr += "(" + (xp-75)/2 + "," + dist[ii] + "),\t";
+
+                    xp += 1;
+                }
+
+                pen.Color = Color.Blue;
+                pen.Width = 2f;
+                xp = 75f;
+                for (int ii = 0; ii < (dist.Length - 1); ii++)
+                {
+                    float yp1 = 550 - (dist[ii] / 100f) * 5;
+                    float yp2 = 550 - (dist[ii+1] / 100f) * 5;
+                    g.DrawLine(pen, xp, yp1, xp + 1, yp2);
+                    xp += 1;
+                }
+            }
+
+            textData.Text = pointsStr;
+            pen.Dispose();
+            g.Dispose();
+            graphPicBox.BackgroundImage = bmp;
+        }
+
+       
+    }
+}
+
